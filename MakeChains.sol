db5f25829a53d04bfe120f247ad02ec68d64447e
Logan Bonney <wakeup2early@gmail.com>
2021-09-17 23:43:48 -0600

    implemented mapping players to shared game data

--- MakeChains.sol
+++ MakeChains.sol
@@ -4,23 +4,20 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-library Library {
-    struct game { // Struct
-        address player1;
-        address player2;
-        string[][] gameState;
-    }
-}
-
 
 contract MakeChains is ERC721, ERC721Enumerable, AccessControl {
     
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
-    using Library for Library.game;
-    mapping(address => Library.game) games;
+    struct Game { // Struct
+        address[2] players;
+        string[3][3] gameState;
+    }
     
-    constructor() ERC721("MyToken", "MTK") {
+    mapping(address => address) gameIds;
+    mapping(address => Game) games;
+    
+    constructor() ERC721("Make Chains", "Chains") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
     }
@@ -33,14 +30,39 @@ contract MakeChains is ERC721, ERC721Enumerable, AccessControl {
         _safeMint(to, tokenId);
     }
     
-    function startGame(address player2) public {
+    function startGame(address _player1, address _player2) public {
+        // require that neither player is already playing a game
+        require(gameIds[_player1] == address(0) && gameIds[_player2] == address(0), "Only one game at once for now!");
         
+        // Set the game
+        gameIds[_player1] = _player1;
+        gameIds[_player2] = _player1;
+        
+        // Set first and second player
+        getGame(_player1).players[0] = _player1;
+        getGame(_player1).players[1] = _player2;
     }
     
     function takeTurn() public {
         
     }
     
+    function getPlayer1(address player) public view returns (address) {
+        return getGame(player).players[0];
+    }
+    
+    function getPlayer2(address player) public view returns (address) {
+        return getGame(player).players[1];
+    }
+    
+    function getGameState(address player) public view returns (string[3][3] memory) {
+        return getGame(player).gameState;
+    }
+    
+    function getGame(address player) private view returns (Game storage) {
+        return games[gameIds[player]];
+    }
+
     // Determines if game is over
     function checkChains() public view returns (bool) {
         return false;
